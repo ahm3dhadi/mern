8326e97d7c75ea7f9ea0a7535f6af3cecbd12f85
Ahmed Elhadi <ahmedelhadi.oh@gmail.com>
2022-02-07 03:10:13 -0800

    add user schema

---
+++ server/models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+// mongoose user model
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
